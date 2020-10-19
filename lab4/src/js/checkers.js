f2eafee354a9cb9dc6b502276375a0812c3feb5c
Belinsky Roman <romanbelinsky99@gmail.com>
2020-10-20 01:23:14 +0600

    1. Rework class Checker 2. Remove class King 3. Rework checkerFactory 4. Implement CheckerBoard 5. Add class Game

--- lab4/src/js/checkers.js
+++ lab4/src/js/checkers.js
@@ -1,39 +1,95 @@
 const whiteChecker = 'w';
 const blackChecker = 'b';
-const whiteKing = 'wk';
-const blackKing = 'bk';
 
 class Checker {
-    constructor(checkerType) {
-        this._checkerType = checkerType;
+    constructor(checkerColor, isKing) {
+        this._checkerColor = checkerColor;
+        this._isKing = isKing;
     }
 
-    get checkerType() {
-        return this._checkerType;
+    changeType() {
+        this._isKing = !this._isKing;
+    }
+
+    get checkerColor() {
+        return this._checkerColor;
+    }
+
+    get isKing() {
+        return this._isKing;
     }
 }
 
-class King extends Checker {
-    constructor(checkerType) {
-        super(checkerType);
-    }
-}
-
-function checkerFactory(checkerType) {
+function checkerFactory(checkerType, isKing) {
     let factory = new Map([
-        [whiteChecker, new Checker(whiteChecker)],
-        [blackChecker, new Checker(blackChecker)],
-        [whiteKing, new King(whiteKing)],
-        [blackKing, new King(blackKing)]
+        [whiteChecker, new Checker(whiteChecker, false)],
+        [blackChecker, new Checker(blackChecker, false)]
     ]);
 
-    return factory.get(checkerType);
+    const checker = factory.get(checkerType);
+
+    if (isKing) {
+        checker.changeType();
+    }
+
+    return checker;
 }
 
 class CheckerBoard {
-    // TODO
     constructor() {
-        this._board = null;
+        this._boardSize = 8;
+        this._board = [];
+
+        for (let i = 0; i < this._boardSize; i++) {
+            let temp = [];
+
+            for (let j = 0; j < this._boardSize; j++) {
+                temp[j] = null;
+            }
+
+            this._board[i] = temp;
+        }
+    }
+
+    getChecker(row, col) {
+        return this._board[row][col];
+    }
+
+    isCheckerColor(row, col, checkerColor) {
+        const checker = this.getChecker(row, col);
+        return checker != null && checkerColor === checker.checkerColor();
+    }
+
+    setChecker(row, col, checker) {
+        this._board[row][col] = checker;
+    }
+
+    removeChecker(row, col) {
+        this.setChecker(row, col, null);
+    }
+}
+
+class Game {
+    constructor(checkerBoard, firstMoveColor) {
+        this._checkerBoard = checkerBoard;
+        this._currentMoveColor = firstMoveColor;
+    }
+
+    move(oldRow, oldCol, newRow, newCol) {
+        if (!this.canMoveTo(oldRow, oldCol, newRow, newCol)) {
+            return;
+        }
+
+        const movingChecker = this._checkerBoard.getChecker(oldRow, oldCol);
+        this._checkerBoard.removeChecker(oldRow, oldCol);
+        this._checkerBoard.setChecker(newRow, newCol, movingChecker);
+
+        this._currentMoveColor = this._currentMoveColor === whiteChecker ? blackChecker : whiteChecker;
+    }
+
+    canMoveTo(oldRow, oldCol, newRow, newCol) {
+        return this._checkerBoard.isCheckerColor(oldRow, oldCol, this._currentMoveColor)
+            && this._checkerBoard.getChecker(newRow, newCol) == null;
     }
 }
 
