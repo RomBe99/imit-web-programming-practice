d2250d232fa34bd3057f5d2683eda4f4ccd89463
Roman <romanbelinsky99@gmail.com>
2020-10-23 02:15:34 +0600

    Reworking checkers (in progress)

--- lab4/src/js/checkers.js
+++ lab4/src/js/checkers.js
@@ -1,14 +1,10 @@
-const whiteChecker = 'w';
-const blackChecker = 'b';
+const white = 'w';
+const black = 'b';
 
 class Checker {
     constructor(checkerColor, isKing) {
         this._checkerColor = checkerColor;
         this._isKing = isKing;
-    }
-
-    changeType() {
-        this._isKing = !this._isKing;
     }
 
     get checkerColor() {
@@ -20,44 +16,24 @@ class Checker {
     }
 }
 
-function checkerFactory(checkerType, isKing) {
+function checkerFactory(checkerColor, isKing) {
     let factory = new Map([
-        [whiteChecker, new Checker(whiteChecker, false)],
-        [blackChecker, new Checker(blackChecker, false)]
+        [white, new Checker(white, isKing)],
+        [black, new Checker(black, isKing)]
     ]);
 
-    const checker = factory.get(checkerType);
-
-    if (isKing) {
-        checker.changeType();
-    }
-
-    return checker;
+    return factory.get(checkerColor);
 }
 
 class CheckerBoard {
     constructor() {
-        this._boardSize = 8;
-        this._board = [];
+        this._boardCol = 8;
+        this._boardRow = 8;
+        this._board = new Array(this._boardRow);
 
-        for (let i = 0; i < this._boardSize; i++) {
-            let temp = [];
-
-            for (let j = 0; j < this._boardSize; j++) {
-                temp[j] = null;
-            }
-
-            this._board[i] = temp;
+        for (let i = 0; i < this._boardRow; i++) {
+            this._board.push(new Array(this._boardCol));
         }
-    }
-
-    getChecker(row, col) {
-        return this._board[row][col];
-    }
-
-    isCheckerColor(row, col, checkerColor) {
-        const checker = this.getChecker(row, col);
-        return checker != null && checkerColor === checker.checkerColor();
     }
 
     setChecker(row, col, checker) {
@@ -65,33 +41,31 @@ class CheckerBoard {
     }
 
     removeChecker(row, col) {
-        this.setChecker(row, col, null);
-    }
-}
-
-class Game {
-    constructor(checkerBoard, firstMoveColor) {
-        this._checkerBoard = checkerBoard;
-        this._currentMoveColor = firstMoveColor;
+        this.setChecker(row, col, undefined);
     }
 
-    move(oldRow, oldCol, newRow, newCol) {
-        if (!this.canMoveTo(oldRow, oldCol, newRow, newCol)) {
-            return;
+    clear() {
+        for (let i = 0; i < this._boardRow; i++) {
+            this._board.push(new Array(this._boardCol));
         }
-
-        const movingChecker = this._checkerBoard.getChecker(oldRow, oldCol);
-        this._checkerBoard.removeChecker(oldRow, oldCol);
-        this._checkerBoard.setChecker(newRow, newCol, movingChecker);
-
-        this._currentMoveColor = this._currentMoveColor === whiteChecker ? blackChecker : whiteChecker;
     }
 
-    canMoveTo(oldRow, oldCol, newRow, newCol) {
-        return this._checkerBoard.isCheckerColor(oldRow, oldCol, this._currentMoveColor)
-            && this._checkerBoard.getChecker(newRow, newCol) == null;
+    get boardCol() {
+        return this._boardCol;
+    }
+
+    get boardRow() {
+        return this._boardRow;
     }
 }
+
+function moves() {
+    let result = [];
+
+    return result;
+}
+
+
 
 function startGame() {
     // TODO Implement a standard checker alignment
